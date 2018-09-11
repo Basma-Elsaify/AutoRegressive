99f27ef7b9728a77c4eeed857d7704b15697bfb8
Basma-Elsaify <basmaelsaify@gmail.com>
2018-09-11 13:59:47 +0400

    Update AR.py
    rename column

--- AR.py
+++ AR.py
@@ -20,8 +20,8 @@ series = Series.from_csv('file.csv', index_col=0, header = 0 )
 # df = pd.read_csv('data/file.csv', delimiter = ';')    
 # df = df.replace({'t': {',': '.'}}, regex=True)
 # df['t'] = df['t'].astype(np.float64)
-# df = df.replace({'deflexion11': {',': '.'}}, regex=True)
-# df['deflexion11'] = df['deflexion11'].astype(np.float64)
+# df = df.replace({'target': {',': '.'}}, regex=True)
+# df['target'] = df['target'].astype(np.float64)
 # df.to_csv('file_export.csv')
 
 series = series.astype(np.float64)
@@ -105,51 +105,3 @@ pyplot.show()
 pyplot.plot(test)
 # pyplot.savefig("ar_test.png")
 pyplot.show()
-
-
-# In[ ]:
-
-
-import warnings
-from pandas import Series
-from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
-
-def evaluate_arima_model(X, arima_order):    
-    train_size = int(len(X) * 0.75)
-    train, test = X[0:train_size], X[train_size:]
-    history = [x for x in train]
-    
-    predictions = list()
-    for t in range(len(test)):
-        model = ARIMA(history, order=arima_order)
-        model_fit = model.fit(disp=0)
-        yhat = model_fit.forecast()[0]
-        predictions.append(yhat)
-        history.append(test[t])
-        
-    error = mean_squared_error(test, predictions)
-    return error
-
-def evaluate_models(dataset, p_values, d_values, q_values):
-    dataset = dataset.astype('float32')
-    best_score, best_cfg = float("inf"), None
-    for p in p_values:
-        for d in d_values:
-            for q in q_values:
-                order = (p,d,q)
-                try:
-                    mse = evaluate_arima_model(dataset, order)
-                    if mse < best_score:
-                        best_score, best_cfg = mse, order
-                    print('ARIMA%s MSE=%.3f' % (order,mse))
-                except:
-                    continue
-    print('Best ARIMA%s MSE=%.3f' % (best_cfg, best_score))
-
-p_values = [1, 4, 8]
-d_values = range(0, 2)
-q_values = range(0, 2)
-warnings.filterwarnings("ignore")
-evaluate_models(series.values, p_values, d_values, q_values)
-
